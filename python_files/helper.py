d6cddcb02008bd3380fcafa42417f81b7a927e7f
Gustaf Bengtz <gbengtz@gmail.com>
2023-06-13 18:54:41 +0200

    Start adding python files for handling data

---
+++ python_files/helper.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3.9
+
+class CookCLI():
+  def __init__(self):
+    # Do something
+  def __enter(self):
+    # If you want to use a contextmanager style
+  def __exit__(self):
+    # If you want to use a contextmanager style
---
+++ python_files/mainfile.py
@@ -0,0 +1,6 @@
+#!/usr/löocal/bin/python3.9
+
+if __name__ == "__main__":
+# Handle argparse here, and create basic structure
+
+# Use library files for read, shoppingcart etc for recipe. That should be linked to the recipe
