6f63261ca40a1488f71ba9a625b885297bda2c37
Gustaf Bengtz <gbengtz@gmail.com>
2023-06-13 22:33:37 +0200

    First commit. Read seems to work fine with one example tested

--- python_files/helper.py
+++ python_files/helper.py
@@ -1,9 +1,120 @@
-#!/usr/local/bin/python3.9
+from pprint import pprint #remove this before commit
+import re
+
+
+def print_metadata(metadata):
+    if not metadata:
+        return
+    print("Metadata!")
+
+def print_ingredients(ingredients):
+    if not ingredients:
+        return
+    
+    # Print header
+    print("Ingredients:")
+
+    # Find longest name, so that formatting can be based on that
+    max_len_name = max([len(item['name']) for item in ingredients])
+    for item in ingredients:
+        if item['type'] == 'ingredient':
+            print(f'    {item["name"].ljust(max_len_name, " ")}    {item["quantity"]} {item["units"]}')
+        else:
+            # This is unexpected. Log this occurance!
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            with open('log.log') as file:
+                file.write(ingredients, encoding='UTF-8')
+
+def print_steps(steps):
+    if not steps:
+         return
+    print("Steps:")
+    index = 1
+    time = 0
+    for step in steps:
+        text = []
+        cookware = []
+        ingredient = []
+        timer = []
+
+        for sub_step in step:
+            #if index == 5:
+             # import pdb; pdb.set_trace()
+            match sub_step['type']:
+                case 'text':
+                    text.append(sub_step['value'])
+                case 'cookware':
+                    text.append(sub_step['name'])
+                    quantity = sub_step.get("quantity", None)
+                    if quantity:
+                        cookware.append(f'{sub_step["name"]}: {quantity}')
+                    else:
+                        cookware.append(sub_step["name"])
+                case 'ingredient':
+                    text.append(sub_step['name'])
+                    ingredient.append(f'{sub_step["name"]}: {sub_step["quantity"]} {sub_step["units"]}')
+                case 'timer':
+                    m = re.match('(?P<match>.*)[\.]', sub_step['quantity'])
+                    if m:
+                        # This is some ugly washing of str -> float -> int via regex.
+                        quantity = m.group('match')
+                    else:
+                        quantity = sub_step['quantity']
+                    time = time + int(quantity)
+                    text.append(f'{quantity} {sub_step["units"]}')
+                    timer.append((quantity, sub_step['units']))
+
+        # Print step
+        print(f'     {index}. {"".join(text)}')
+        # Print cookware
+        if cookware:
+            print(f'        [{"; ".join(cookware)}]')
+        # Print ingredients
+        if ingredient:
+            print(f'        [{"; ".join(ingredient)}]')
+        else:
+            print(f'        [-]')
+        index = index + 1
+
+    print()
+    print(f'Estimated time: {time}')
+    
+    
+
+def print_recipe(recipe):
+    print_metadata(recipe['metadata'])
+    print()
+    print_ingredients(recipe['ingredients'])
+    print()
+    print_steps(recipe['steps'])
+    print()
+
+class RecipeTree():
+  
+  def find_recipes():
+    # Do a tree walk and list all .cook files and what folders they are located in.
+    # This should perhaps be turned into a structure of some sort, so that it is
+    # easy to take a look at it in the webbrowser?
+    print("Hej")
+
+  def __repr__(self):
+    # Print the tree
+    print("Hej")
+
+  def _populate_tree(self):
+    # Populate items in this class
+    print("Hej")
+
+
+
 
 class CookCLI():
   def __init__(self):
     # Do something
+    print("Hej")
   def __enter(self):
     # If you want to use a contextmanager style
+    print("Hej")
   def __exit__(self):
     # If you want to use a contextmanager style
+    print("Hej")
--- python_files/mainfile.py
+++ python_files/mainfile.py
@@ -1,6 +1,81 @@
-#!/usr/löocal/bin/python3.9
+#!C:\Users\Gustaf\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\python.exe
+
+from cooklang import *
+import sys
+from helper import print_recipe
+
+def print_help():
+    print('Usage: mainfile.py [seed|recipe|shopping-list|server] <options>')
+    print('seed: <root-path to .cook files>')
+    print('recipe: read, ...')
+    print('shopping-list: <file1.cook> <file2.cook> ...')
+    print('server does not need any arguments')
+    print()
+
+
 
 if __name__ == "__main__":
-# Handle argparse here, and create basic structure
+    command = sys.argv[1]
+    early_exit = False
+    if command not in ['seed', 'recipe', 'shopping-list', 'server']:
+        print(f'Command "{command}" not found.')
+        early_exit = True
+    if  len(sys.argv) < 3 and command != 'server':
+        print('Too few arguments')
+        early_exit = True
+        
+    if early_exit:
+        print()
+        print_help()
+        exit(-1)
 
-# Use library files for read, shoppingcart etc for recipe. That should be linked to the recipe
+    match command:
+        case 'seed':
+            # seed: populate the RecipeTree($PWD)'''
+            print("Seed!")
+            path = sys.argv[2]
+        case 'recipe':
+            '''
+            recipe <cmd>,
+              read: Print information to terminal
+                Metadata: ...
+
+                Ingredients: ...
+
+                Steps: ...
+            '''
+            if len(sys.argv) < 4:
+                print(f'Wrong number of arguments for {command}')
+                print_help()
+                exit(-1)
+            sub_cmd = sys.argv[2]
+            
+            match sub_cmd:
+                case 'read':
+                    # Handle read
+                    recipe = parseRecipe(sys.argv[3])
+                    print_recipe(recipe)
+                case _:
+                    print("Everything except read")
+        case 'shopping-list':
+            '''
+            shopping-list <any number of recipe paths>:
+            Loops through the ingredients in the recipes and adds them to a shopping list
+            Then prints how many of each item, and in what section
+            '''
+            print("Shopping list printing")
+            recipe_paths = []
+            index = 2
+            while sys.argv[index]:
+                recipe_paths.append(sys.argv[index])
+                index = index + 1
+            print(f'Got {index-2} recipes!')
+        case 'server':
+            '''
+            server:
+                Start a webserver. Find out if you want to use FLASK or if there exists something better.
+            '''
+            print("This is gonna be a tricky one. Starting server!")
+    
+    # Handle argparse here, and create basic structure
+    # Use library files for read, shoppingcart etc for recipe. That should be linked to the recipe
